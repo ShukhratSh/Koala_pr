7acdd1703a919332e914e2226edb15834f59c7a9
shukhrat811 <shukhrat811@gmail.com>
2017-06-21 12:59:05 +0500

    Update Koala_project.py

--- Koala_project.py
+++ Koala_project.py
@@ -1,233 +1,190 @@
+
 # coding: utf-8
 
-# In[100]:
+# In[116]:
 
-get_ipython().magic('pylab notebook')
-from __future__ import print_function
-import datacube
+import pandas as pd
+import numpy as np
 import xarray as xr
-from datacube.storage import masking
-from datacube.storage.masking import mask_to_dict
+import datacube
 from datacube.helpers import ga_pq_fuser
-from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib import pyplot as plt
-import matplotlib.dates
+from datacube.api import make_mask
+from affine import Affine
 import fiona
-import shapely
-import shapely.geometry
-from shapely.geometry import shape
 import rasterio
+import rasterio.features
+import matplotlib.pyplot as plt
 
 
-# In[101]:
+# In[117]:
 
-dc = datacube.Datacube(app='NDVI,SAVI calculation based on the observed points')
-
-
-# In[102]:
-
-#This defines the function that converts a linear vector file into a string of x,y coordinates
-def geom_query(geom, geom_crs='EPSG:4326'):
-    """
-    Create datacube query snippet for geometry
-    """
-    return {
-        'x': (geom.bounds[0], geom.bounds[2]),
-        'y': (geom.bounds[1], geom.bounds[3]),
-        'crs': geom_crs
-    }
-
-def warp_geometry(geom, crs_crs, dst_crs):
-    """
-    warp geometry from crs_crs to dst_crs
-    """
-    return shapely.geometry.shape(rasterio.warp.transform_geom(crs_crs,dst_crs, shapely.geometry.mapping(geom)))
+# Parameters for the extract
+startDate = '1995-01-01'
+numMonths = 180
+shapefileName = '/home/147/ss8137/Documents/koala_obs2/tile222.shp' 
 
 
-def conv(data, geom, method='nearest', tolerance=None):
-    """
-    
-    """
-    
-    points = list(zip(*[geom.coords[0]]))
-    indexers = {
-        data.crs.dimensions[0]: list(points[1]),
-        data.crs.dimensions[1]: list(points[0])        
-    }
-    return data.sel_points(xr.DataArray(points, name='points'),
-                           method=method,
-                           tolerance=tolerance,
-                           **indexers)
-                        
+# In[118]:
+
+def load_multiple_masked_dataset(platforms, query, bands):
+    nbars = []
+    fcs = []
+    for platform in platforms:
+        product_name = '{}_{}_albers'.format(platform, 'nbar')
+        ds = dc.load(product=product_name, measurements=bands, group_by='solar_day', **query)
+        
+        if not ds:
+            continue
+        product_fc= '{}_{}_albers'.format(platform, 'fc')
+        fcov = dc.load(product = product_fc, measurements=pfc_measurements, group_by='solar_day', **query)
+        
+        if not fcov:
+            continue
+        mask_product = '{}_{}_albers'.format(platform, 'pq')
+        sensor_pq = dc.load(product=mask_product, fuse_func=ga_pq_fuser, group_by='solar_day', **query)
+        if not sensor_pq:
+            continue
+        cloud_free = make_mask(sensor_pq.pixelquality, ga_good_pixel=True)
+        ds = ds.where(cloud_free)
+        ds['product'] = ('time', np.repeat(product_name, ds.time.size))
+        fcov = fcov.where(cloud_free)
+        fcov['product'] = ('time', np.repeat(product_fc, fcov.time.size))
+        nbars.append(ds)
+        fcs.append(fcov)
+
+    if len(nbars)>0:
+        combined_nbar = xr.concat(nbars, dim='time')
+        combined_nbar = combined_nbar.isel(time=combined_nbar.time.argsort())
+        return combined_nbar
+    elif len(fcov)>0:
+        combined_fc = xr.concat(fcs, dim='time')
+        combined_fc = combined_fc.isel(time=combined_fc.time.argsort())
+        return combined_fc
+    else:
+        return None
 
 
-# In[103]:
+# In[119]:
 
-#### DEFINE SPATIOTEMPORAL RANGE AND BANDS OF INTEREST
-#Select polyline, replacing /g/... with /*your_path_here*/your_file.shp
-
-vec_fname = '/home/147/ss8137/Documents/koala_obs/koala_obs_1995_2009.shp' 
-
-with fiona.open(vec_fname) as src:
-    geom = shape(src[0]['geometry'])
-
-#Define temporal range
-start_of_epoch = '1995-01-01'
-#need a variable here that defines a rolling 'latest observation'
-end_of_epoch =  '2009-12-31'
-
-#Define wavelengths/bands of interest, remove this kwarg to retrieve all bands
-bands_of_interest = [#'blue',
-                     #'green',
-                     'red', 
-                     'nir',
-                     #'swir1', 
-                     #'swir2'
-                     ]
-
-#Define sensors of interest
-sensors  = ['ls7','ls5']
-
-query = {'time': (start_of_epoch, end_of_epoch),}
-query.update(geom_query(geom)) 
-query['crs'] = 'EPSG:4326'
+dc = datacube.Datacube(app='point-ndvi-savi-recipe')
 
 
-# In[104]:
+# In[120]:
 
-print (query)
-
-
-# In[105]:
-
-#Group PQ by solar day to avoid idiosyncracies of N/S overlap differences in PQ algorithm performance
-pq_albers_product = dc.index.products.get_by_name(sensors[0]+'_pq_albers')
-valid_bit = pq_albers_product.measurements['pixelquality']['flags_definition']['contiguous']['bits']
+# Generate a list of monthly dates
+dateList = pd.date_range(startDate, periods=numMonths+1, freq='1M').tolist()
+print("Processing data from " + str(dateList[0]) + " to " + str(dateList[-1]))
 
 
-# In[106]:
+# In[121]:
 
-#Define which pixel quality artefacts you want removed from the results
-mask_components = {'cloud_acca':'no_cloud',
-'cloud_shadow_acca' :'no_cloud_shadow',
-'cloud_shadow_fmask' : 'no_cloud_shadow',
-'cloud_fmask' :'no_cloud',
-'blue_saturated' : False,
-'green_saturated' : False,
-'red_saturated' : False,
-'nir_saturated' : False,
-'swir1_saturated' : False,
-'swir2_saturated' : False,
-'contiguous':True}
+# Read in the points. This has to be EPSG:3577. 
+with fiona.open(shapefileName, "r") as shapefile:
+    # Get the total extent of all the data in the shapefile
+    bounds = shapefile.bounds 
+    # Extract the individual geometries
+    geoms = [feature["geometry"] for feature in shapefile]
+    numGeoms = len(geoms)
+    # You could extract anything here - I'm just grabbing a list of the ID fields
+    ids = [feature["id"] for feature in shapefile]
+    print("Order of Geometries is" + str(ids))
 
 
-# In[107]:
+# In[122]:
 
-#retrieve the NBAR and PQ for the spatiotemporal range of interest
+# Initialise Arrays
 
-#Retrieve the NBAR and PQ data for sensor n
-sensor_clean = {}
-for sensor in sensors:
-    #Load the NBAR and corresponding PQ
-    sensor_nbar = dc.load(product= sensor+'_nbar_albers', group_by='solar_day', measurements = bands_of_interest,  **query)
-    sensor_pq = dc.load(product= sensor+'_pq_albers', group_by='solar_day', fuse_func=ga_pq_fuser, **query)
-    #grab the projection info before masking/sorting
-    crs = sensor_nbar.crs
-    crswkt = sensor_nbar.crs.wkt
-    affine = sensor_nbar.affine
-    #This line is to make sure there's PQ to go with the NBAR
-    sensor_nbar = sensor_nbar.sel(time = sensor_pq.time)
-    #Apply the PQ masks to the NBAR
-    cloud_free = masking.make_mask(sensor_pq, **mask_components)
-    good_data = cloud_free.pixelquality.loc[start_of_epoch:end_of_epoch]
-    sensor_nbar = sensor_nbar.where(good_data)
-    sensor_clean[sensor] = sensor_nbar
+NDVI = np.zeros((numMonths,numGeoms))
+SAVI = np.zeros((numMonths,numGeoms))
 
 
-# In[108]:
+# In[123]:
+
+# Use 5-7 Landsats.
+platforms = ['ls5', 'ls7']
+# Only Want the bands to calculate NDVI
+bands_of_interest = ['nir','red']
+
+pfc_measurements=['BS']
 
 
-#Concatenate the data from different sensors together and sort by time
-nbar_clean = xr.concat(sensor_clean.values(), dim='time')
-time_sorted = nbar_clean.time.argsort()
-nbar_clean = nbar_clean.isel(time=time_sorted)
-nbar_clean.attrs['crs'] = crs
-nbar_clean.attrs['affine'] = affine
+# In[124]:
 
-#Extract the observation data volume
-geom_o = warp_geometry(geom, query['crs'], crs.wkt)
-obs = conv(nbar_clean, geom_o)
+# Loop through the months. 
+for i in range(numMonths):
+    print(dateList[i])
+    # Setup The Query using the bounds of the shapefile and a 1 month interval
+    query = {'time': (dateList[i],dateList[i+1]),'x': (bounds[0], bounds[2]),'y': (bounds[1], bounds[3]) ,'crs':'EPSG:3577'}
+    # Load all the data for the month from the DataCube
+    combined_nbar = load_multiple_masked_dataset(platforms, query, bands_of_interest)
+    if combined_nbar is not None:
+        # Compute the per pixel NDVI  and SAVI over the period
+        #lfac = combined_fc.BS
+        monthlyNDVI = ((combined_nbar.nir - combined_nbar.red) / (combined_nbar.nir + combined_nbar.red))
+        # Extract each of the geometries 
+        for j in range(numGeoms):
+            # Build an analysis mask using a crazy 1-liner. Doing this every loop is dumb but it's the premature optimization + redability thing. Hey, it works
+            mask=rasterio.features.geometry_mask([geoms[j]],out_shape=combined_nbar.geobox.shape,transform=combined_nbar.geobox.affine,all_touched=False,invert=True)
+            # Extract the NDVI
+            NDVI[i,j] = monthlyNDVI.where(mask)
+            # Count the number of pixels of ndvi in the mask area
+            countOfPixels_ndvi = np.sum(np.isfinite(monthlyNDVI.where(mask)))
+            
+    combined_fc = load_multiple_masked_dataset(platforms, query, bands_of_interest)
+    if combined_fc is not None:
+        lfac = combined_fc.BS
+        monthlySAVI = ((combined_nbar.nir - combined_nbar.red) / (combined_nbar.nir + combined_nbar.red + lfac))*(1 + lfac)
+        for j in range(numGeoms):
+        # Build an analysis mask using a crazy 1-liner. Doing this every loop is dumb but it's the premature optimization + redability thing. Hey, it works
+            mask=rasterio.features.geometry_mask([geoms[j]],out_shape=combined_fc.geobox.shape,transform=combined_fc.geobox.affine,all_touched=False,invert=True)
+            # Extract the SAVI
+            SAVI[i, j] = monthlySAVI.where(mask)
+            # Count the number of pixels of savi in the mask area
+            countOfPixels_savi = np.sum(np.isfinite(monthlySAVI.where(mask)))
+    else:
+        # There is noData. 
+        SAVI[i,:] = np.nan
+#here is the error
+ValueError                                Traceback (most recent call last)
+<ipython-input-133-6abff40978eb> in <module>()
+     16             mask=rasterio.features.geometry_mask([geoms[j]],out_shape=combined_nbar.geobox.shape,transform=combined_nbar.geobox.affine,all_touched=False,invert=True)
+     17             # Extract the NDVI
+---> 18             NDVI[i,j] = monthlyNDVI.where(mask)
+     19             # Count the number of pixels of ndvi in the mask area
+     20             countOfPixels_ndvi = np.sum(np.isfinite(monthlyNDVI.where(mask)))
+
+ValueError: setting an array element with a sequence.
+
+# In[ ]:
+
+# Save the results to a file
+np.savetxt("/home/147/ss8137/Documents/koala_obs2/NDVI_t222.csv", NDVI, delimiter=',')
+np.savetxt("/home/147/ss8137/Documents/koala_obs2/SAVI_t222.csv", SAVI, delimiter=',')
 
 
-# In[109]:
+# In[ ]:
 
-print('The number of time slices at this location is '+ str(nbar_clean.red.shape[0]))
-
-
-# In[110]:
-
-#select time slice of interest - this is trial and error until you get a decent image
-time_slice_i = 280
-rgb = nbar_clean.isel(time =time_slice_i).to_array(dim='color').sel(color=['red', 'nir']).transpose('y', 'x', 'color')
-#rgb = nbar_clean.isel(time =time_slice).to_array(dim='color').sel(color=['swir1', 'nir', 'green']).transpose('y', 'x', 'color')
-fake_saturation = 4500
-clipped_visible = rgb.where(rgb<fake_saturation).fillna(fake_saturation)
-max_val = clipped_visible.max(['y', 'x'])
-scaled = (clipped_visible / max_val)
-
-
-# In[111]:
-
-#View the vector points on the imagery
-fig = plt.figure(figsize =(6,6))
-plt.scatter(x=geom_o.x, y=geom_o.y, c='r') #turn this on or off to show location of transect
-plt.imshow(scaled, interpolation = 'nearest',
-           extent=[scaled.x.min(), scaled.x.max(), 
-                   scaled.y.min(), scaled.y.max()])
-
-date_ = nbar_clean.time[time_slice_i]
-plt.title(date_.astype('datetime64[D]'))
-plt.show()
+# Plot the results in a plot. Actually, 2 plots. Well, 2 subplots in 1 plot. Probaby just one plot I guess if you want to be specific.
+# This would be cooler with the xarray magic plottingness if I could work out how to make  NDVI etc as xarrays. 
+f, (ax1, ax2) = plt.subplots(2, 1,figsize=(16,8),sharex=True)
+# Plot the two metrics
+ax1.plot(dateList[1:],NDVI)
+ax2.plot(dateList[1:],SAVI)
+# Add grids because grids look scientific
+ax1.grid(True)
+ax2.grid(True)
+# Add in titles and labels after the reviewer complained
+ax1.set_title(' NDVI')
+ax2.set_title('SAVI')
+ax2.set_xlabel('Date')
+ax1.set_ylabel('NDVI')
+ax2.set_ylabel('SAVI')
+# Add a legend. Or two.
+ax1.legend(ids, loc='upper left')
+ax2.legend(ids, loc='upper left')
 
 
 # In[ ]:
 
 
----------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-<ipython-input-111-cf28e3136062> in <module>()
-      4 plt.imshow(scaled, interpolation = 'nearest',
-      5            extent=[scaled.x.min(), scaled.x.max(), 
-----> 6                    scaled.y.min(), scaled.y.max()])
-      7 
-      8 date_ = nbar_clean.time[time_slice_i]
 
-/g/data/v10/public/modules/agdc-py3-env/20170427/envs/agdc/lib/python3.6/site-packages/matplotlib/pyplot.py in imshow(X, cmap, norm, aspect, interpolation, alpha, vmin, vmax, origin, extent, shape, filternorm, filterrad, imlim, resample, url, hold, data, **kwargs)
-   3156                         filternorm=filternorm, filterrad=filterrad,
-   3157                         imlim=imlim, resample=resample, url=url, data=data,
--> 3158                         **kwargs)
-   3159     finally:
-   3160         ax._hold = washold
-
-/g/data/v10/public/modules/agdc-py3-env/20170427/envs/agdc/lib/python3.6/site-packages/matplotlib/__init__.py in inner(ax, *args, **kwargs)
-   1890                     warnings.warn(msg % (label_namer, func.__name__),
-   1891                                   RuntimeWarning, stacklevel=2)
--> 1892             return func(ax, *args, **kwargs)
-   1893         pre_doc = inner.__doc__
-   1894         if pre_doc is None:
-
-/g/data/v10/public/modules/agdc-py3-env/20170427/envs/agdc/lib/python3.6/site-packages/matplotlib/axes/_axes.py in imshow(self, X, cmap, norm, aspect, interpolation, alpha, vmin, vmax, origin, extent, shape, filternorm, filterrad, imlim, resample, url, **kwargs)
-   5116                               resample=resample, **kwargs)
-   5117 
--> 5118         im.set_data(X)
-   5119         im.set_alpha(alpha)
-   5120         if im.get_clip_path() is None:
-
-/g/data/v10/public/modules/agdc-py3-env/20170427/envs/agdc/lib/python3.6/site-packages/matplotlib/image.py in set_data(self, A)
-    547         if (self._A.ndim not in (2, 3) or
-    548                 (self._A.ndim == 3 and self._A.shape[-1] not in (3, 4))):
---> 549             raise TypeError("Invalid dimensions for image data")
-    550 
-    551         self._imcache = None
-
-TypeError: Invalid dimensions for image data
